3e3b2bb442b17ebe9814a42f976f967c414aaeb0
Aleson De Pano <alesondepano@gmail.com>
2026-03-23 15:21:11 +0800

    Updated e

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,6 +40,7 @@ const Home = () => {
         setLoading(true);
         setError(null);
         
+      
         const API_URL = import.meta.env.VITE_API_URL || "https://ason-armory-backend.onrender.com";
         const res = await fetch(`${API_URL}/api/products`);
         
